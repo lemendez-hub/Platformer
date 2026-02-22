29cc03e63b8387f928c5456986273af28118ebe9
lemendez-hub <lemendez@csumb.edu>
2026-02-21 19:29:53 -0800

    3D Animation
    Followed along in class with the 3D animation.

--- Assets/Platformer/Scripts/CharacterDriver.cs
+++ Assets/Platformer/Scripts/CharacterDriver.cs
@@ -1,84 +1,90 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class CharacterDriver : MonoBehaviour
+[RequireComponent(typeof(CharacterController))]
+public class CharacterControllerDriver : MonoBehaviour
 {
-    public float groundAcceleration = 15f;
+    [Header("Ground Config")]
     public float walkSpeed = 5f;
     public float runSpeed = 10f;
+    public float groundAcceleration = 15f;
 
+    [Header("Air Config")]
+    public float airAcceleration = 0.5f;
     public float apexHeight = 4.5f;
     public float apexTime = 0.5f;
-    Vector2 _velocity;
+
     CharacterController _controller;
-    Quaternion facingRight;
-    Quaternion facingLeft;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Animator _animator;
+    float _xVelocity;
+    float _yVelocity;
+
+    Quaternion _facingRight;
+    Quaternion _facingLeft;
+
+    void Awake()
     {
-        facingRight = Quaternion.identity;
-        facingLeft = Quaternion.Euler(0f, 180f, 0f);
         _controller = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
+        _facingRight = Quaternion.Euler(0f, 90f, 0f);
+        _facingLeft = Quaternion.Euler(0f, -90f, 0f);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Poll Input 
         float direction = 0f;
-        if(Keyboard.current.dKey.isPressed)
-        {
-            direction += 1f;
-        }
-        if(Keyboard.current.aKey.isPressed)
-        {
-            direction -= 1f;
-        }
+        if (Keyboard.current.aKey.isPressed) direction -= 1f;
+        if (Keyboard.current.dKey.isPressed) direction += 1f;
+
         bool jumpPressedThisFrame = Keyboard.current.spaceKey.wasPressedThisFrame;
         bool jumpHeld = Keyboard.current.spaceKey.isPressed;
-        float gravityModifier = 1f;
-        if (_controller.isGrounded)
+        bool runHeld = Keyboard.current.leftShiftKey.isPressed;
+
+        if (_controller.isGrounded) // Ground-based movement
         {
             if (direction != 0f)
             {
-                if (Mathf.Sign(direction) != Mathf.Sign(_velocity.x))
-                {
-                    _velocity.x = 0f;
-                }
-                _velocity.x += direction * groundAcceleration * Time.deltaTime;
-                _velocity.x = Mathf.Clamp(_velocity.x, -walkSpeed, walkSpeed);
-
-                transform.rotation = (direction > 0f) ? facingRight : facingLeft;
+                _xVelocity += direction * groundAcceleration * Time.deltaTime;
+                transform.rotation = (direction > 0f) ? _facingRight : _facingLeft;
             }
             else
-            {
-                _velocity.x = Mathf.MoveTowards(_velocity.x, 0f, groundAcceleration * Time.deltaTime);
-            }
+                _xVelocity = Mathf.MoveTowards(_xVelocity, 0f, groundAcceleration * Time.deltaTime);
+
             if (jumpPressedThisFrame)
             {
-                _velocity.y = 2f * apexHeight / apexTime;
+                float jumpImpulse = 2f * apexHeight / apexTime;
+                _yVelocity = jumpImpulse;
             }
+            else if (_yVelocity < 0f)
+                _yVelocity = -1f; // Keep it sticky (i.e. stay grounded)
         }
-        else
+        else // Air-based movement
         {
-            if (!jumpHeld)
-            {
-                gravityModifier = 2f;
-            }
-        }
-        float gravity = 2f * apexHeight / (apexTime * apexTime);
-        _velocity.y -= gravity * gravityModifier * Time.deltaTime;
-        float deltaX = _velocity.x * Time.deltaTime;
-        float deltaY = _velocity.y * Time.deltaTime;
+            float gravity = -2f * apexHeight / (apexTime * apexTime);
 
-        Vector3 deltaPosition = new(deltaX, deltaY, 0f);
-        CollisionFlags collision = _controller.Move(deltaPosition);
-        if((collision & CollisionFlags.CollidedAbove) != 0)
-        {
-            _velocity.y = -1f;
+            if (!jumpHeld)
+                gravity *= 2f;
+
+            _yVelocity += gravity * Time.deltaTime;
+            _xVelocity += direction * airAcceleration * Time.deltaTime;
         }
-        if ((collision & CollisionFlags.CollidedSides) != 0)
-        {
-            _velocity.x = 0f;
-        }
+
+        // Speed Clamping
+        float xMaxSpeed = runHeld ? runSpeed : walkSpeed;
+        _xVelocity = Mathf.Clamp(_xVelocity, -xMaxSpeed, xMaxSpeed);
+
+        // Apply velocity to change position
+        Vector3 deltaPosition = new Vector3(_xVelocity, _yVelocity, 0f) * Time.deltaTime;
+        CollisionFlags collisionFlags = _controller.Move(deltaPosition);
+
+        // Reset velocities based on collisions
+        if ((collisionFlags & CollisionFlags.Above) != 0 && _yVelocity > 0f)
+            _yVelocity = 0f;
+
+        if ((collisionFlags & CollisionFlags.Sides) != 0)
+            _xVelocity = 0f;
+        _animator.SetFloat("Speed", Mathf.Abs(_xVelocity));
+        _animator.SetBool("Grounded", _controller.isGrounded);
     }
 }
